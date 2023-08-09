9e17948d2e5cf5cc6dcb0f7926a684e9eee5f9c4
Yohaï-Eliel BERREBY <yohai-eliel.berreby@routine.co>
2023-08-08 21:48:23 -0400

    test: use wasm backend

--- semsearch-jsoo/semsearch-jsoo/tests/jsoo.ml
+++ semsearch-jsoo/semsearch-jsoo/tests/jsoo.ml
@@ -1,14 +1,17 @@
+let sbert_model = "all-MiniLM-L6-v2"
+(* let sbert_model = "paraphrase-MiniLM-L3-v2" *)
+
 let test_root = Sys.getcwd ()
 
 (* FIXME: there has to be a better way than this horror *)
+(* Using Dune dependencies and source_tree doesn't work on parent directories, though. *)
 let root = test_root ^ "../../../../../"
 
 (* TODO: proper path handling *)
-let vocabUrl = root ^ "node_modules/bert-tokenizer/assets/vocab.json"
-
-let json_path =
-  root ^ "exported/js/" ^ "sentence-transformers-all-MiniLM-L6-v2/"
-  ^ "model.json"
+let vocab = root ^ "node_modules/bert-tokenizer/assets/vocab.json"
+let exported_path = root ^ "exported/"
+let tfjs_path = exported_path ^ "js/" ^ "sentence-transformers-" ^ sbert_model
+let json_path = tfjs_path ^ "/model.json"
 
 (* Load list of lines, stripped of whitespace *)
 let sentences : string List.t =
@@ -30,7 +33,7 @@ let top_n n lst =
 
 let test_queries =
   [
-    "uni";
+    "schoolwork";
     "work";
     "paperwork";
     "languages";
@@ -42,17 +45,18 @@ let test_queries =
   ]
 
 let f () =
+  let backend = Tfjs.Backend.Wasm in
+  let%lwt () = Tfjs.Backend.set backend in
   let cur_s = Unix.gettimeofday in
   let start = cur_s () in
-  let%lwt model = Semsearch_jsoo.load_local_model json_path in
+  let handler = tfjs_path |> Tfjs.IO.local_fs_handler in
+  let%lwt model = Tfjs.load_graph_model handler in
   Fmt.pr "Model loaded in %.3fs\n@." (cur_s () -. start);
-  let tokenizer = Sbert_jsoo.SBERT.Tokenizer.with_vocab vocabUrl in
-
+  let tokenizer = Sbert_jsoo.SBERT.Tokenizer.with_vocab vocab in
   let start = cur_s () in
   let encoded_sentences = Sbert_jsoo.SBERT.encode model tokenizer sentences in
   Fmt.pr "Encoded %d sentences in %.3fs\n" (List.length sentences)
     (cur_s () -. start);
-
   let find_with_query base_sentence =
     let start = cur_s () in
     let base_encoded =
@@ -73,7 +77,6 @@ let f () =
     let end_search = cur_s () in
     Fmt.pr "Searched in %.3fs\n" (end_search -. start_search)
   in
-
   Base.List.iter test_queries ~f:find_with_query;
   Lwt.return_unit
 
