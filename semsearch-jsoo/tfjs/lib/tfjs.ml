bf7bb62db379fc8285b8dacd0ef001e2d7e1dae2
Yohaï-Eliel BERREBY <yohai-eliel.berreby@routine.co>
2023-08-08 21:33:49 -0400

    Tfjs: richer backend story

--- semsearch-jsoo/tfjs/lib/tfjs.ml
+++ semsearch-jsoo/tfjs/lib/tfjs.ml
@@ -14,14 +14,31 @@ module Tensor = struct
     Js.to_array output
 end
 
-let tf = lazy (Js.Unsafe.js_expr "require('@tensorflow/tfjs')")
-let tfn = lazy (Js.Unsafe.js_expr "require('@tensorflow/tfjs-node')")
+let require pkg =
+  lazy
+    (Fmt.pr "JS require('%s')@." pkg;
+     Js.Unsafe.fun_call
+       (Js.Unsafe.js_expr "require")
+       [| Js.string pkg |> Js.Unsafe.inject |])
+
+let tf = require "@tensorflow/tfjs"
+let tfn = require "@tensorflow/tfjs-node"
+
+(** This is required to register Node-specific backends. *)
 
 let load_graph_model handler =
   Js.Unsafe.fun_call
     (Lazy.force tf)##.loadGraphModel
     [| Js.Unsafe.inject handler |]
   |> Promise.to_lwt
+
+let load_saved_model path =
+  Js.Unsafe.fun_call
+    (Lazy.force tfn)##.node##.loadSavedModel
+    [| Js.string path |> Js.Unsafe.inject |]
+  |> Promise.to_lwt
+
+let ready () = Js.Unsafe.fun_call (Lazy.force tf)##.ready [||] |> Promise.to_lwt
 
 let tensor2d data shape dtype =
   Js.Unsafe.fun_call
@@ -53,8 +70,60 @@ let reshape tensor (shape : int array) =
   Js.Unsafe.meth_call tensor "reshape" [| Js.Unsafe.inject shape_js |]
 
 module IO = struct
+  (* Custom IOHandler that does not require tfjs-node. *)
+  let local_fs_handler path =
+    let constr = Js.Unsafe.js_expr "LocalFSHandler" in
+    new%js constr (path |> Js.string |> Js.Unsafe.inject)
+
   let fileSystem path =
     Js.Unsafe.fun_call
       (Lazy.force tfn)##.io##.fileSystem
       [| path |> Js.string |> Js.Unsafe.inject |]
+end
+
+module Backend = struct
+  type t =
+    | Pure_js  (** You will die of old age before this finishes running *)
+    | Wasm
+        (** Low-latency and easily portable across Node.js, browser, Electron *)
+    | Native_tensorflow  (** C++ *)
+    | Webgpu
+        (** Meant to be the highest-performing backend, and should work under Node *)
+    | Webgl  (** Use the GPU on older browsers *)
+    | Other of string (* React... *)
+
+  let to_string = function
+    | Pure_js -> "cpu" (* Yes, I know. *)
+    | Native_tensorflow -> "node"
+    | Webgpu -> "webgpu"
+    | Webgl -> "webgl"
+    | Wasm -> "wasm"
+    | Other s -> s
+
+  let of_string = function
+    | "cpu" -> Pure_js
+    | "node" -> Native_tensorflow
+    | "webgpu" -> Webgpu
+    | "webgl" -> Webgl
+    | "wasm" -> Wasm
+    | s -> Other s
+
+  let wasm_backend = require "@tensorflow/tfjs-backend-wasm"
+
+  let set backend : unit Lwt.t =
+    (* XXX: it's a more complicated story for some other backends. *)
+    (match backend with
+    | Native_tensorflow -> ignore (Lazy.force tfn)
+    | Wasm -> ignore (Lazy.force wasm_backend)
+    | _ -> ());
+
+    let backend = to_string backend in
+
+    Js.Unsafe.fun_call
+      (Lazy.force tf)##.setBackend
+      [| Js.string backend |> Js.Unsafe.inject |]
+    |> Promise.to_lwt
+
+  let get () : string =
+    Js.Unsafe.fun_call (Lazy.force tf)##.getBackend [||] |> Js.to_string
 end
